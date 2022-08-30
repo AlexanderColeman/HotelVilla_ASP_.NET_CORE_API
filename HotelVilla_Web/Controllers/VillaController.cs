565b6ee59382605b6663331f92679a3abf1b611c
Alexander Coleman <alexanderjcoleman01@gmail.com>
2022-08-30 17:00:38 -0500

    Added Villa Service

---
+++ HotelVilla_Web/Controllers/VillaController.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using HotelVilla_Web.Models;
+using HotelVilla_Web.Models.Dto;
+using HotelVilla_Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace HotelVilla_Web.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly IVillaService _villaService;
+        private readonly IMapper _mapper;
+
+        public VillaController(IVillaService villaService,IMapper mapper)
+        {
+            _villaService = villaService;
+            _mapper = mapper;
+        }
+        public async Task<IActionResult> IndexVilla()
+        {
+            List<VillaDTO> list = new();
+
+            var response = await _villaService.GetAllAsync<APIResponse>();
+            if(response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject <List<VillaDTO>>(Convert.ToString(response.Result));
+            }
+            return View(list);
+        }
+    }
+}
--- HotelVilla_Web/Services/VillaService.cs
+++ HotelVilla_Web/Services/VillaService.cs
@@ -1,4 +1,5 @@
 ﻿using HotelVilla_Utility;
+using HotelVilla_Web.Models;
 using HotelVilla_Web.Models.Dto;
 using HotelVilla_Web.Services.IServices;
 
@@ -7,62 +8,57 @@ namespace HotelVilla_Web.Services
     public class VillaService : BaseService, IVillaService
     {
         private readonly IHttpClientFactory _cleintFacotry;
-        private string villaUrl; 
+        private string villaUrl;
 
-        public VillaService(IHttpClientFactory cleintFacotry, IConfiguration  configuration) : base(cleintFacotry)
+        public VillaService(IHttpClientFactory cleintFacotry, IConfiguration configuration) : base(cleintFacotry)
         {
             _cleintFacotry = cleintFacotry;
             villaUrl = configuration.GetValue<string>("ServiceUrls:VillaAPI");
         }
         public Task<T> CreateAsync<T>(VillaCreateDTO dto)
         {
-            return SendAsync<T>(new Models.APIRequest()
+            return SendAsync<T>(new APIRequest()
             {
                 ApiType = SD.ApiType.POST,
                 Data = dto,
-                Url = $"{villaUrl}/api/villaAPI"
-
+                Url = villaUrl + "/api/villaAPI"
             });
         }
 
         public Task<T> DeleteAsync<T>(int id)
         {
-            return SendAsync<T>(new Models.APIRequest()
+            return SendAsync<T>(new APIRequest()
             {
                 ApiType = SD.ApiType.DELETE,
-                Url = $"{villaUrl}/api/villaAPI/{id}"
-
+                Url = villaUrl + "/api/villaAPI/" + id
             });
         }
 
         public Task<T> GetAllAsync<T>()
         {
-            return SendAsync<T>(new Models.APIRequest()
+            return SendAsync<T>(new APIRequest()
             {
                 ApiType = SD.ApiType.GET,
-                Url = $"{villaUrl}/api/villaAPI"
-
+                Url = villaUrl + "/api/villaAPI"
             });
         }
 
         public Task<T> GetAsync<T>(int id)
         {
-            return SendAsync<T>(new Models.APIRequest()
+            return SendAsync<T>(new APIRequest()
             {
-                ApiType = SD.ApiType.DELETE,
-                Url = $"{villaUrl}/api/villaAPI/{id}"
-
+                ApiType = SD.ApiType.GET,
+                Url = villaUrl + "/api/villaAPI/" + id
             });
         }
 
         public Task<T> UpdateAsync<T>(VillaUpdateDTO dto)
         {
-            return SendAsync<T>(new Models.APIRequest()
+            return SendAsync<T>(new APIRequest()
             {
                 ApiType = SD.ApiType.PUT,
                 Data = dto,
-                Url = $"{villaUrl}/api/villaAPI"
-
+                Url = villaUrl + "/api/villaAPI/" + dto.Id
             });
         }
     }
