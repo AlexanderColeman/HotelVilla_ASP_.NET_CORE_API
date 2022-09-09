dc91e04ec154a121dbfed38fd73b6272cb6f246b
Alexander Coleman <alexanderjcoleman01@gmail.com>
2022-09-08 20:45:53 -0500

    Added Authentication service

---
+++ HotelVilla_Web/Services/AuthService.cs
@@ -0,0 +1,39 @@
+﻿using HotelVilla_Utility;
+using HotelVilla_Web.Models;
+using HotelVilla_Web.Models.Dto;
+using HotelVilla_Web.Services.IServices;
+
+namespace HotelVilla_Web.Services
+{
+	public class AuthService :BaseService, IAuthService
+	{
+        private readonly IHttpClientFactory _cleintFacotry;
+        private string villaUrl;
+
+    public AuthService(IHttpClientFactory cleintFacotry, IConfiguration configuration) : base(cleintFacotry)
+    {
+        _cleintFacotry = cleintFacotry;
+        villaUrl = configuration.GetValue<string>("ServiceUrls:VillaAPI");
+    }
+	
+		public Task<T> LoginAsync<T>(LoginRequestDTO obj)
+		{
+            return SendAsync<T>(new APIRequest()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = obj,
+                Url = villaUrl + "/api/UsersAuth/login"
+            });
+        }
+
+		public Task<T> RegisterAsync<T>(UserDTO obj)
+		{
+            return SendAsync<T>(new APIRequest()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = obj,
+                Url = villaUrl + "/api/UsersAuth/register"
+            });
+        }
+	}
+}
---
+++ HotelVilla_Web/Services/IServices/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using HotelVilla_Web.Models.Dto;
+
+namespace HotelVilla_Web.Services.IServices
+{
+	public interface IAuthService
+	{
+		Task<T> LoginAsync<T>(LoginRequestDTO obj);
+		Task<T> RegisterAsync<T>(UserDTO obj);
+	}
+}
